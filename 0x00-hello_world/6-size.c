842043c3349466318b121ee1fc018491a4b513c2
meron6 <khozameron5@gmail.com>
2023-10-05 09:10:23 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,6 @@
 
 int main(void)
 {
-	printf("with proper programar, but the outcome is a piece of art,\n")
-	return (0);
+printf("with proper programar, but the outcome is a piece of art,\n")
+return (0);
 }
